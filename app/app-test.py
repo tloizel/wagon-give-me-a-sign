993d0ac3c94a4a73997061fc471bf7e0c70faa03
Thomas Loizel <thomas.loizel@gmail.com>
2023-07-02 09:08:42 +0200

    not much left

--- app/app-test.py
+++ app/app-test.py
@@ -1,15 +1,15 @@
 import streamlit as st
 import cv2
 import mediapipe as mp
-from model import load_model_ml, predict_model_ml
+# from model import load_model_ml, predict_model_ml
 import av
 from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
-from game import random_letter, countdown_timer
-import ipdb
+from game import random_letter
+# import ipdb
 from data_proc import preproc_predict
 import threading
 from registry import load_model
-import urllib.parse
+# import urllib.parse
 import time
 import os
 
@@ -30,9 +30,10 @@ hands = mp_hands.Hands(static_image_mode=False,
 @st.cache_data()
 def patience_while_i_load_the_model():
     # Load and return the model
-    return load_model_ml()
+    # return load_model(ml=True, model_name='random_forest_1')
+    return load_model(ml=True, model_name='model_base_testing')
 
-print('loading model')
+
 # model = load_model(ml=True, model_name='random_forest_1')
 # model = load_model_ml()
 model = patience_while_i_load_the_model()
@@ -42,10 +43,11 @@ def video_frame_callback(frame):
     img = frame.to_ndarray(format="bgr24")
     with lock:
         img_container["img"] = img
-    return frame
+    img = process(img)[0]
+    return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 
-def process_out_of_class(image):
+def process(image):
 
     image.flags.writeable = False
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -55,100 +57,70 @@ def process_out_of_class(image):
 
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
+    answer = 'No hand'
+
     if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(
+                image,
+                hand_landmarks,
+                mp_hands.HAND_CONNECTIONS,
+                mp_drawing_styles.get_default_hand_landmarks_style(),
+                mp_drawing_styles.get_default_hand_connections_style())
+
+        #for rectangle later
+        H, W, _ = image.shape
+        x_ = []
+        y_ = []
+        for i in range(len(hand_landmarks.landmark)):
+            x = hand_landmarks.landmark[i].x
+            y = hand_landmarks.landmark[i].y
+            x_.append(x)
+            y_.append(y)
+
+        #draw rectangle around hand
+        x1 = int(min(x_) * W) - 10
+        y1 = int(min(y_) * H) - 10
+        x2 = int(max(x_) * W) + 10
+        y2 = int(max(y_) * H) + 10
+        cv2.rectangle(image, (x1, y1), (x2, y2), (0, 0, 0), 2)
+
+
         #predict and show prediction
         coords_df = preproc_predict(image, {'mp_hands': mp_hands, 'hands': hands, 'results': results})
+
         if coords_df is None:
             pass
         else:
             # pred = model.predict_proba(coords_df)
             pred = model.predict(coords_df)
 
+
             res = 'none' if pred is None else pred
 
-            predicting_letter = res[0].capitalize()
+            # res = res[0].tolist()
+            # max_value = max(res)
+            # max_index = res.index(max_value)
 
-            return predicting_letter
+            # if max_value>0.90:
+            if res is not None:
+                # answer = f"{ALPHABET[max_index].capitalize()} ({round(max_value,2)*100}%)"
+                answer = res[0].capitalize()
 
-class VideoProcessor:
-
-    def process(self, image):
-
-        image.flags.writeable = False
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        results = hands.process(image)
-        # Draw the hand annotations on the image.
-        image.flags.writeable = True
-
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-
-        if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(
-                    image,
-                    hand_landmarks,
-                    mp_hands.HAND_CONNECTIONS,
-                    mp_drawing_styles.get_default_hand_landmarks_style(),
-                    mp_drawing_styles.get_default_hand_connections_style())
-
-            #for rectangle later
-            H, W, _ = image.shape
-            x_ = []
-            y_ = []
-            for i in range(len(hand_landmarks.landmark)):
-                x = hand_landmarks.landmark[i].x
-                y = hand_landmarks.landmark[i].y
-                x_.append(x)
-                y_.append(y)
-
-            #draw rectangle around hand
-            x1 = int(min(x_) * W) - 10
-            y1 = int(min(y_) * H) - 10
-            x2 = int(max(x_) * W) + 10
-            y2 = int(max(y_) * H) + 10
-            cv2.rectangle(image, (x1, y1), (x2, y2), (0, 0, 0), 2)
-
-
-            #predict and show prediction
-            coords_df = preproc_predict(image, {'mp_hands': mp_hands, 'hands': hands, 'results': results})
-            if coords_df is None:
-                pass
             else:
-                # pred = model.predict_proba(coords_df)
-                pred = model.predict(coords_df)
+                answer = "No letter"
 
 
-                res = 'none' if pred is None else pred
+        cv2.putText(image,
+                    answer,
+                    (x1, y1 - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1.3,
+                    (0, 0, 0),
+                    3,
+                    cv2.LINE_AA)
 
-                # res = res[0].tolist()
-                # max_value = max(res)
-                # max_index = res.index(max_value)
-
-                # if max_value>0.90:
-                if res is not None:
-                    # answer = f"{ALPHABET[max_index].capitalize()} ({round(max_value,2)*100}%)"
-                    answer = res[0].capitalize()
-
-                else:
-                    answer = "No letter"
-
-
-                cv2.putText(image,
-                            answer,
-                            (x1, y1 - 10),
-                            cv2.FONT_HERSHEY_SIMPLEX,
-                            1.3,
-                            (0, 0, 0),
-                            3,
-                            cv2.LINE_AA)
-
-        return image
-
-    def recv(self, frame):
-            img = frame.to_ndarray(format="bgr24")
-            img = self.process(img)
-            return av.VideoFrame.from_ndarray(img, format="bgr24")
+    return image, answer
 
 
 def most_common(lst):
@@ -161,7 +133,6 @@ def main():
     RTC_CONFIGURATION = RTCConfiguration(
     {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
     )
-
 
     #Variables
     goal = random_letter()
@@ -188,7 +159,6 @@ def main():
             mode=WebRtcMode.SENDRECV,
             rtc_configuration=RTC_CONFIGURATION,
             media_stream_constraints={"video": True, "audio": False},
-            video_processor_factory= VideoProcessor,
             async_processing=True,
             video_frame_callback=video_frame_callback,
             )
@@ -203,7 +173,7 @@ def main():
             if img is None:
                 continue
 
-            pred = process_out_of_class(img)
+            pred = process(img)[1]
 
             if pred is None:
                 continue
@@ -228,7 +198,7 @@ def main():
 
         bar = st.progress(0)
         score_text = st.empty()
-        score_text.text(f"Fastest time to get 10 letters wins 🏆")
+        score_text.text(f"Fastest time to get {win} letters wins 🏆")
 
         goal_text = st.empty()
         goal_text.text(f"Show us the letter {goal}")
@@ -240,7 +210,6 @@ def main():
             mode=WebRtcMode.SENDRECV,
             rtc_configuration=RTC_CONFIGURATION,
             media_stream_constraints={"video": True, "audio": False},
-            video_processor_factory= VideoProcessor,
             async_processing=True,
             video_frame_callback=video_frame_callback,
             )
@@ -255,7 +224,7 @@ def main():
             if img is None:
                 continue
 
-            pred = process_out_of_class(img)
+            pred = process(img)[1]
 
             if pred is None:
                 continue
