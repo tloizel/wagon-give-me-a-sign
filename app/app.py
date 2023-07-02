1eab2230bf759835bd695ac2d5b3bc35d7f733dd
Thomas Loizel <thomas.loizel@gmail.com>
2023-07-02 10:06:01 +0200

    path

--- app/app.py
+++ app/app.py
@@ -1,6 +1,3 @@
-import sys
-sys.path.append("..")  # Add the root directory to the Python path
-
 import streamlit as st
 import cv2
 import mediapipe as mp
@@ -9,13 +6,15 @@ import av
 from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
 from game import random_letter
 # import ipdb
-from data_proc import preproc_predict
 import threading
-from registry import load_model
 # import urllib.parse
 import time
 import os
 
+import sys
+sys.path.append("..")  # Add the root directory to the Python path
+from registry import load_model
+from data_proc import preproc_predict
 
 
 lock = threading.Lock()
